ee0ebbb2efde1ec002d38c9dc79c0573b9dc3281
shubham yadav <shubham.yadav@in.ey.com>
2021-04-20 21:02:29 +0530

    bootstart added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import {Alert} from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            {[
+                'primary',
+                'secondary',
+                'success',
+                'danger',
+                'warning',
+                'info',
+                'light',
+                'dark',
+            ].map((variant, idx) => (
+                <Alert key={idx} variant={variant}>
+                    This is a {variant} alert—check it out!
+                </Alert>
+            ))}  </div>
+    );
 }
 
 export default App;
